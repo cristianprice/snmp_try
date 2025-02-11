892cb58f009c3301117459ba39c9acfbb3e3bb05
cristianp <cristianp@ceragon.com>
2025-02-11 15:11:40 +0200

    Initial files.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snmpagent.net/sag
+
+go 1.23.2
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "ber.h"
+#include <cstring>
+
+void bcopy(char *src, char *dst, int nbytes)
+{
+    memcpy(dst, src, nbytes);
+}
+
+void main()
+{
+}
